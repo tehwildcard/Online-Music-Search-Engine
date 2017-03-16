b2e6a03cb7fa7567d00d62a6b0c65908f6b633e2
Clay Walters <crwaltersjr@gmail.com>
2017-03-16 13:56:17 -0400

    ajax requests

--- js/Search.js
+++ js/Search.js
@@ -3,53 +3,67 @@
  */
 //var webServiceURL = ;
 
-
+var results = document.getElementById('results');
+var artistResults = document.getElementById('results');
 function start(){
     document.getElementById("submit").addEventListener("click", search, false);
     document.getElementById("search").addEventListener("click", search, false);
     //document.getElementById("artist").addEventListener("click", METHOD, false);
     document.getElementById("results").style.display = 'none';
+
+
 }
 
-function search(){
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            myObj = JSON.parse(this.responseText);
-            document.getElementById("demo").innerHTML = myObj.name;
+var searchAlbums = function (artist) {
+    $.ajax({
+        url: 'https://api.spotify.com/v1/search',
+        data: {
+            q: artist,
+            type: 'album'
+        },
+        success: function (response) {
+            artistResults.innerHTML = response;
         }
-    };
-    xmlhttp.open("GET", "json_demo.txt", true);
-    xmlhttp.send();
+    });
+};
 
-    displaySearchResults();
-    displayGenre();
-    displayRelatedArtists();
-    displayAlbumCover();
+
+document.getElementById('artist-search').addEventListener('submit', function (e) {
+    e.preventDefault();
+    searchAlbums(document.getElementById('artist').value);
+}, false);
+
+
+
+
+/*    displaySearchResults();
+ displayGenre();
+ displayRelatedArtists();
+ displayAlbumCover();*/
 }
 
-function displaySearchResults() {
-    var myObj = { "name":"John", "age":30, "car":null };
-    for (x in myObj) {
-        document.getElementById("results").innerHTML += myObj[x] + "<br>";
-    }
-}
-function displayGenre() {
-    var myObj = { "name":"John", "age":30, "car":null };
-    for (x in myObj) {
-        document.getElementById("genre").innerHTML += myObj[x] + "<br>";
-    }
+/*function displaySearchResults() {
+ var myObj = { "name":"John", "age":30, "car":null };
+ for (x in myObj) {
+ document.getElementById("results").innerHTML += myObj[x] + "<br>";
+ }
+ }
+ function displayGenre() {
+ var myObj = { "name":"John", "age":30, "car":null };
+ for (x in myObj) {
+ document.getElementById("genre").innerHTML += myObj[x] + "<br>";
+ }
 
-}
-function displayRelatedArtists() {
-    var myObj = { "name":"John", "age":30, "car":null };
-    for (x in myObj) {
-        document.getElementById("related").innerHTML += myObj[x] + "<br>";
-    }
-}
-function displayAlbumCover() {
-    var myObj = { "name":"John", "age":30, "car":null };
-    for (x in myObj) {
-        document.getElementById("albumCover").innerHTML += myObj[x] + "<br>";
-    }
-}
+ }
+ function displayRelatedArtists() {
+ var myObj = { "name":"John", "age":30, "car":null };
+ for (x in myObj) {
+ document.getElementById("related").innerHTML += myObj[x] + "<br>";
+ }
+ }
+ function displayAlbumCover() {
+ var myObj = { "name":"John", "age":30, "car":null };
+ for (x in myObj) {
+ document.getElementById("albumCover").innerHTML += myObj[x] + "<br>";
+ }
+ }*/
